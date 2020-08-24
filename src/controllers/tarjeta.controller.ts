79b96c78d4db162319d3e47e03a91539c2c13015
Raul Granados <00138816@uca.edu.sv>
2020-08-24 12:16:58 -0600

    Corrigiendo error en consulta de tarjetas

--- src/controllers/tarjeta.controller.ts
+++ src/controllers/tarjeta.controller.ts
@@ -7,11 +7,20 @@ import { response } from '../libs/functions'
 /** TARJETAS USUARIO :: TOKEN */
 export const getAllByUsuario = async (req: Request, res: Response): Promise<Response> => {
   try {
-    const tarjetas = await Tarjeta.find({ usuario: req.user["id"] }).sort({ ultima_usada: true });
+    const tarjeta = await Tarjeta.find({ usuario: req.user["id"] });
 
-    return res.status(201).json(
-      response(201, "Ejecutado con exito", true, null, tarjetas)
-    );
+    if (!tarjeta) {
+      const tarjetas = await Tarjeta.find({ usuario: req.user["id"] }).sort({ ultima_usada: true });
+
+      return res.status(201).json(
+        response(201, "Ejecutado con exito", true, null, tarjetas)
+      );
+
+    } else {
+      return res.status(201).json(
+        response(201, "Ejecutado con exito", true, null, tarjeta)
+      );
+    }
   } catch (error) {
     return res.status(404).json(
       response(404, null, false, 'Algo salio mal: ' + error, null)
@@ -21,72 +30,72 @@ export const getAllByUsuario = async (req: Request, res: Response): Promise<Resp
 
 /** REGISTRO DE TARJETA */
 export const nuevo = async (
-    req: Request,
-    res: Response
+  req: Request,
+  res: Response
 ): Promise<Response> => {
-    if (!req.body) {
-        return res
-        .status(404)
-        .json(response(404, null, false, 'Campos incompletos.', null));
-    }
-    try {
-      const nuevaTarjeta = new Tarjeta(req.body);
-      nuevaTarjeta.usuario = req.user['id']
+  if (!req.body) {
+    return res
+      .status(404)
+      .json(response(404, null, false, 'Campos incompletos.', null));
+  }
+  try {
+    const nuevaTarjeta = new Tarjeta(req.body);
+    nuevaTarjeta.usuario = req.user['id']
 
-      await nuevaTarjeta.save();
+    await nuevaTarjeta.save();
 
-      return res.status(201).json(
-        response(201, "Ejecutado con exito", true, null, nuevaTarjeta)
-      );
-    } catch (error) {
-      return res.status(404).json(
-        response(404, null, false, 'Algo salio mal: ' + error, null)
-      );
-    }
+    return res.status(201).json(
+      response(201, "Ejecutado con exito", true, null, nuevaTarjeta)
+    );
+  } catch (error) {
+    return res.status(404).json(
+      response(404, null, false, 'Algo salio mal: ' + error, null)
+    );
+  }
 };
 
 /** ACTUALIZACION DE TARJETA :: RECIBE EL ID */
 export const actualizar = async (
-    req: Request,
-    res: Response
+  req: Request,
+  res: Response
 ): Promise<Response> => {
-    if (!req.body || !req.params.id) {
-        return res
-        .status(404)
-        .json(response(404, null, false, 'Campos incompletos o faltan parametros en la URL.', null));
-    }
-    try {
-      await Tarjeta.findByIdAndUpdate(req.params.id, { ...req.body });
+  if (!req.body || !req.params.id) {
+    return res
+      .status(404)
+      .json(response(404, null, false, 'Campos incompletos o faltan parametros en la URL.', null));
+  }
+  try {
+    await Tarjeta.findByIdAndUpdate(req.params.id, { ...req.body });
 
-      return res.status(201).json(
-        response(201, "Ejecutado con exito", true, null, null)
-      );
-    } catch (error) {
-      return res.status(404).json(
-        response(404, null, false, 'Algo salio mal: ' + error, null)
-      );
-    }
+    return res.status(201).json(
+      response(201, "Ejecutado con exito", true, null, null)
+    );
+  } catch (error) {
+    return res.status(404).json(
+      response(404, null, false, 'Algo salio mal: ' + error, null)
+    );
+  }
 };
 
 /** ELIMINACION DE TARJETA :: RECIBE EL ID */
 export const eliminar = async (
-    req: Request,
-    res: Response
+  req: Request,
+  res: Response
 ): Promise<Response> => {
-    if (!req.params.id) {
-        return res
-        .status(404)
-        .json(response(404, null, false, 'Faltan parametros en la URL.', null));
-    }
-    try {
-      await Tarjeta.findByIdAndDelete(req.params.id);
+  if (!req.params.id) {
+    return res
+      .status(404)
+      .json(response(404, null, false, 'Faltan parametros en la URL.', null));
+  }
+  try {
+    await Tarjeta.findByIdAndDelete(req.params.id);
 
-      return res.status(200).json(
-        response(200, "Ejecutado con exito", true, null, null)
-      );
-    } catch (error) {
-      return res.status(404).json(
-        response(404, null, false, 'Algo salio mal: ' + error, null)
-      );
-    }
+    return res.status(200).json(
+      response(200, "Ejecutado con exito", true, null, null)
+    );
+  } catch (error) {
+    return res.status(404).json(
+      response(404, null, false, 'Algo salio mal: ' + error, null)
+    );
+  }
 };
