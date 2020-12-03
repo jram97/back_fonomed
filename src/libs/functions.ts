124bc8f294d5a00bab386a1ce6039ed091946494
Raul Granados <00138816@uca.edu.sv>
2020-12-03 16:54:38 -0600

    Arreglando bug

--- src/libs/functions.ts
+++ src/libs/functions.ts
@@ -317,6 +317,7 @@ export const filtrarCitasCaducadas = (citas: any, estado:any) => {
     console.log("Fecha al pasarla a moment y ponerle hora", mFechaCita);
 
     console.log("Pendientes", now.diff(mFechaCita, "minutes"));
+    console.log(cita[estado]);
     if (((now.diff(mFechaCita, "minutes") <= 0)) && cita[estado] == "Pendiente") {
       return cita;
     }
