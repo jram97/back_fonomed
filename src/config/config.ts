56fb61ee88038b40ad36816946b85a582191780b
Oscar Efrain Martinez <42881580+oemartinez2018@users.noreply.github.com>
2021-09-21 10:50:12 -0600

    updating for local deployment
    updating for local deplotment

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,8 +10,8 @@ export default {
   /** DB CONFIG */
   DB: {
     //URI: process.env.MONGODB_URI_LOCAL,
-    URI: process.env.MONGODB_URI_PROD_FONOMED,
-    //URI: process.env.MONGODB_URI_DOCKER_ROOTS,
+    //URI: process.env.MONGODB_URI_PROD_FONOMED,
+    URI: process.env.MONGODB_URI_DOCKER_ROOTS,
     USER: process.env.MONGODB_USER,
     PASSWORD: process.env.MONGODB_PASSWORD
   }
